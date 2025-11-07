0c40cb830d3ea9304c3913c72ce6cc3ebec1b14e
syukranDev <syukran.dev@gmail.com>
2025-11-07 15:12:05 +0800

    save progress

--- frontend/mytrackerwallet_v2_frontend/src/pages/Dashboard/Home.jsx
+++ frontend/mytrackerwallet_v2_frontend/src/pages/Dashboard/Home.jsx
@@ -1,14 +1,42 @@
 import React from 'react'
 import DashboardLayout from '../../components/layouts/DashboardLayout'
 import { useUserAuth } from '../../hooks/useUserAuth'
+import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axiosInstance from '../../utils/axiosInstance'
+import { API_PATH } from '../../utils/apiPaths'
 
 const Home = () => {
   useUserAuth()
 
+  const navigate = useNavigate()
+
+  const [dashboardData, setDashboardData] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  const fetchDashboardData = async () => {
+    try {
+      setLoading(true)
+      const response = await axiosInstance.get(API_PATH.DASHBOARD.GET_DASHBOARD_DATA)
+      
+      if (response.data) {
+        setDashboardData(response.data)
+      }
+    } catch (error) {
+      console.error('Error fetching dashboard data:', error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchDashboardData()
+  }, [])
+
   return (
     <DashboardLayout>
       <div className="my-5 mx-auto">
-        Home fuckjer
+        Home 
       </div>
     </DashboardLayout>
   )
